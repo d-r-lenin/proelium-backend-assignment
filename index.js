f83ea81df64f5e34721d2fcdb24990dab89c8a80
d-r-lenin <drlenin2002@gmail.com>
2022-08-19 11:52:27 +0530

    auth middleware, Controller object are created

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-require("dotenv").config();
+require("dotenv").config(); // Load .env file
+
+//Loading dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-const User = require('./models/Users');
+//Loading userControl object as 'uc'
+const uc = require("./helpers/UserControl");
+
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
+//Loading middlewares
+const { auth } = require("./helpers/middlewares");
+
+// using middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Connecting to database
 mongoose.connect(process.env.DB_STRING, (e) => {
+    //Check if there is an error
     if (e) {
         console.log(e);
         return;
     }
+    //only if there is no error then start the server 
     app.listen(PORT, () => {
         console.log("on port::" + PORT);
     })
@@ -19,7 +35,90 @@ mongoose.connect(process.env.DB_STRING, (e) => {
 });
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/", auth, (req, res) => {
+    res.send("WellCome!!");
 });
 
+//to add a User or Admin
+app.post('/add',async(req,res)=>{
+    const data = req.body;
+
+    const user = await uc.add(data);
+    if(user === null){
+        res.status(400).json({
+            message:"User already exist"
+        })
+    }
+    res.status(200).json(user);
+});
+
+//to get a User or Admin
+app.get('/view',(req,res)=>{
+    const id = req.query.id;
+    uc.get(id).then((user)=>{
+        res.status(200).json({
+            user:user
+        });
+    }).catch((e)=>{
+        res.status(400).json({
+            message:"User not found"
+        });
+    });
+});
+
+//to update User or admin
+app.put('/update',(req,res)=>{
+    const id = req.query.id;
+    const data = req.body;
+    uc.update(id,data);
+    res.status(200).json({
+        message:"User updated"
+    });
+});
+
+
+//to get Access token
+app.post('/get/token', async (req,res)=>{
+    const { email, password } = req.body;
+    //check if email and password are correct 
+    try{
+        //if correct then generate token
+        const userData = await uc.check(email , password);
+
+        //if not exist then throw error to catch block
+        if(userData === null) throw new Error("User not found");
+        
+        const token = jwt.sign({
+                id:userData._id,
+                role:userData.role
+            },
+            process.env.JWT_SECRET
+        );
+        
+        //send token to user
+        res.status(200).json({
+            token:token
+        });
+
+    }catch(e){
+        res.status(400).json({
+            message:"User not found"
+        });
+    };
+});
+
+//get all users and admins
+app.get('/get/all', auth, async(req,res)=>{
+    //check if user is admin
+    if(req.user.role === 'admin'){
+        const users = await uc.getAll();
+        res.status(200).json({
+            users:users
+        });
+    }else{
+        //if user is not admin then send error message
+        res.status(403).json({
+            message:"Authorization required"
+        });
+    }
+})
